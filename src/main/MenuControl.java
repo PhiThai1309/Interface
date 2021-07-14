0f7d2f3340966b70f54c0661328f37c7de458c3d
PhiThai1309 <s3878070@rmit.edu.vn>
2021-07-14 16:37:26 +0700

    Menu Controller added Remove unnecessary element in homescreen block

---
+++ src/main/MenuControl.java
@@ -0,0 +1,35 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuControl {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
+
+    public void menuCategories(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Categories.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void menuHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("homescreen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
