292e6facac3bbdbada5b0e31863c89e3b790f36f
PhiThai1309 <s3878070@rmit.edu.vn>
2021-07-14 23:22:39 +0700

    Add visualize to Menus Add COVID NEWS and controller

--- src/main/MenuControl.java
+++ src/main/MenuControl.java
@@ -31,5 +31,12 @@ public class MenuControl {
         stage.show();
     }
 
+    public void menuCovid(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Covid.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
